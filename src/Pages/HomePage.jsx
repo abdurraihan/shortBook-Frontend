c08681f9c627cb79b6896a9a75c8f003ecd49e5c
abdurraihan898@gmail.com <abdurraihan@abdurs-MacBook-Air.local>
2024-10-14 16:19:17 +0600

    show all postes in homePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,49 @@
+import { useEffect, useReducer } from "react";
+import { actions } from "../action/action";
+import PostList from "../components/posts/PostList";
 import { useAuth } from "../hook/useAuth";
+import useAxios from "../hook/useAxios.js";
+import { initialState, postReducer } from "../reducers/postReducer";
 
 export default function HomePage() {
   const { auth } = useAuth();
-  console.log(auth);
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+
+    const fetchPost = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+
+        // Check if the request was successful
+        if (response.status === 200) {
+          dispatch({ type: actions.post.DATA_FETCHED, data: response.data });
+        }
+      } catch (error) {
+        console.log(error);
+
+        dispatch({ type: actions.post.DATA_FETCH_ERROR, error: error.message });
+      }
+    };
+
+    fetchPost();
+  }, [api]);
+
+  if (state?.loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (state?.error) {
+    return <div>Error: {state.error}</div>;
+  }
+
   return (
-    <>
-      <p> home page</p>
-    </>
+    <div>
+      <PostList posts={state?.posts}></PostList>
+    </div>
   );
 }
